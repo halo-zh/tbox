6ead536f7afad2dc70e9da52e40559aea8c8042b
hao.zhang10@cn.bosch.com <hao.zhang10@cn.bosch.com>
2022-09-23 16:41:02 +0800

    update .gitignore

--- Tbox_EC600/MDK-ARM/Tbox_EC600/jansson_config.d
+++
@@ -1,2 +0,0 @@
-tbox_ec600\jansson_config.o: RTE\Data_Exchange\jansson_config.c
-tbox_ec600\jansson_config.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- Tbox_EC600/MDK-ARM/Tbox_EC600/startup_stm32f105xc.d
+++
@@ -1,1 +0,0 @@
-tbox_ec600\startup_stm32f105xc.o: startup_stm32f105xc.s
